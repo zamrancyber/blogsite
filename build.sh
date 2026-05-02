e426a1f94b12dcbc76926f5fd6a1bae9ee51174d
zamrancyber <mirkingbangulzai123@gmail.com>
2026-05-02 16:48:31 +0500

    deployed ready

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
--- myblog/urls.py
+++ myblog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-# Serve media files in development
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
